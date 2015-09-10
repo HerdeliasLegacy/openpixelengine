90daf608831b9e7a581104483a98dfdf76b529a0
skad <skadprog@gmail.com>
2015-09-10 14:55:40 +0200

    Add Fullscreen mode

--- src/main/java/com/herdeliaslegacy/runnergameengine/Activity/MainActivity.java
+++ src/main/java/com/herdeliaslegacy/runnergameengine/Activity/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.herdeliaslegacy.runnergameengine.R;
 
@@ -16,6 +17,22 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "test");
+    }
+
+    //change the fullscreen mode
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        View decorView = getWindow().getDecorView();
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+
     }
 
     @Override
