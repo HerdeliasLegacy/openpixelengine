2fe1a321a7fab01e634e7fff7856697d53932da4
skad <skadprog@gmail.com>
2015-09-11 15:06:41 +0200

    Remove fullscreen code and set fullscreen with style theme file

--- src/main/java/com/herdeliaslegacy/runnergameengine/Activity/MainActivity.java
+++ src/main/java/com/herdeliaslegacy/runnergameengine/Activity/MainActivity.java
@@ -26,23 +26,6 @@ public class MainActivity extends Activity implements Button.OnClickListener {
 
     }
 
-    //change the fullscreen mode
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        View decorView = getWindow().getDecorView();
-        if (hasFocus) {
-            decorView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
-
-    }
-
-
     /**
      * Start level for default but neet to be overiden into the final game
      * @param view
