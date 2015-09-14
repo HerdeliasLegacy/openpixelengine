2645a81ba66c9bf1258d9046ff63310a2d52df8d
skad <skadprog@gmail.com>
2015-09-14 14:39:09 +0200

    Add passeur class for txt and json

---
+++ src/main/java/com/herdeliaslegacy/runnergameengine/Utils/JsonParser.java
@@ -0,0 +1,31 @@
+package com.herdeliaslegacy.runnergameengine.Utils;
+
+import android.content.Context;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+/**
+ * Created by skad on 14/09/15.
+ */
+public class JsonParser{
+
+    private JsonParser() {
+    }
+
+    /**
+     * Read a json file and return a json Object from it
+     * @param context
+     * @param fileName
+     * @return
+     * @throws IOException
+     * @throws JSONException
+     */
+    public static JSONObject getFileContent(Context context,String fileName) throws IOException, JSONException {
+        String txtcontent = TxtParser.getFileContent(context,fileName);
+        return new JSONObject(txtcontent);
+    }
+
+}
