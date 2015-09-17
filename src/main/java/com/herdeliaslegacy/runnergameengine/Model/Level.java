5370873121f2f9776283bcf5430c8e00e3ecc8e6
skad <skadprog@gmail.com>
2015-09-17 12:01:10 +0200

    Add decors element list and randomize the first screen of decors

--- src/main/java/com/herdeliaslegacy/runnergameengine/Model/Level.java
+++ src/main/java/com/herdeliaslegacy/runnergameengine/Model/Level.java
@@ -1,5 +1,9 @@
 package com.herdeliaslegacy.runnergameengine.Model;
 
+import android.util.Log;
+
+import com.herdeliaslegacy.runnergameengine.Utils.MathUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
@@ -10,7 +14,15 @@ import java.util.Observable;
 public class Level extends Observable {
     public static final String TAG = "Level";
     private static Level mInstance;
+    private static int mMaxXDraw = 0;
+
+    /** Player */
     private Player mPlayer;
+    /** SpriteObject list for the decors wich will be displayed on the screen */
+    private List<MovingSpriteObject> mDecors = new ArrayList<MovingSpriteObject>();
+
+    /** SpriteObject list with only the element composing the decor. This list will never be drawed */
+    private List<DecorsElement> mDecorsListElements = new ArrayList<DecorsElement>();
 
     private Level() {
     }
@@ -29,6 +41,11 @@ public class Level extends Observable {
         return mInstance;
     }
 
+
+    public static void setmMaxXDraw(int screenWidth) {
+        mMaxXDraw = screenWidth;
+    }
+
     /**
      * Retrieve all the sprites!
      * Note: The order matters as the first object is drawn first (followers will be "above")
@@ -39,8 +56,36 @@ public class Level extends Observable {
         List<SpriteObject> spriteList = new ArrayList<SpriteObject>();
 
         spriteList.add(mPlayer);
+        spriteList.addAll(mDecors);
 
         return spriteList;
+    }
+
+
+    /**
+     * Generate the begening of the level for start running
+     */
+    public void generateLevelStart()
+    {
+        int i = 0;
+        while(i < mMaxXDraw)
+        {
+            i = addRandomElementToDecor(i);
+        }
+    }
+
+    /**
+     * Add a new Element to the DecoList
+     */
+    private int addRandomElementToDecor(int posx){
+        int sizeDecors = mDecors.size()-1;
+        int pos = MathUtils.randomInt(mDecorsListElements.size());
+        MovingSpriteObject element = new MovingSpriteObject(mDecorsListElements.get(pos));
+        Vector2D elementPos = new Vector2D(posx,700);
+        element.setPosition(elementPos);
+        mDecors.add(element);
+        posx += element.getWidth();
+        return posx;
     }
 
     @Override
@@ -61,13 +106,6 @@ public class Level extends Observable {
     }
 
     /**
-     * Update methode for refreshing the level
-     */
-    public void update(){
-
-    }
-
-    /**
      * Add an item to the level
      *
      * @param newObject The SpriteObject to add to our level
@@ -75,8 +113,20 @@ public class Level extends Observable {
     public void add(SpriteObject newObject) {
         if (newObject instanceof Player) {
             mPlayer = (Player) newObject;
+        }else if (newObject instanceof DecorsElement){
+            mDecorsListElements.add((DecorsElement) newObject);
         }
+
+
         setChanged();
         notifyObservers();
     }
+
+    /**
+     * Update methode for refreshing the level
+     */
+    public void update(){
+
+    }
+
 }
