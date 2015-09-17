749545bc936452fec0c37e25e7c3fd1e61558d68
skad <skadprog@gmail.com>
2015-09-17 11:59:26 +0200

    Merge LevelControler with Level Activities

--- src/main/java/com/herdeliaslegacy/runnergameengine/Activity/Level_Activity.java
+++ src/main/java/com/herdeliaslegacy/runnergameengine/Activity/Level_Activity.java
@@ -2,12 +2,22 @@ package com.herdeliaslegacy.runnergameengine.Activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
-import com.herdeliaslegacy.runnergameengine.Controler.LevelControler;
+import com.herdeliaslegacy.runnergameengine.Model.DecorsElement;
 import com.herdeliaslegacy.runnergameengine.Model.Level;
+import com.herdeliaslegacy.runnergameengine.Model.Player;
+import com.herdeliaslegacy.runnergameengine.Model.Vector2D;
 import com.herdeliaslegacy.runnergameengine.R;
+import com.herdeliaslegacy.runnergameengine.Utils.FileUtils;
+import com.herdeliaslegacy.runnergameengine.Utils.JsonParser;
 import com.herdeliaslegacy.runnergameengine.View.LevelView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -38,7 +48,51 @@ public class Level_Activity extends Activity implements Observer {
      * Load Resource
      */
     public void loadResource(){
-        mLevel = LevelControler.loadLevel(this,null);
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        mLevel.setmMaxXDraw(metrics.widthPixels);
+        loadLevelFromFile("0");
+
+        mLevel.generateLevelStart();
+
+    }
+
+    private void loadLevelFromFile(String number){
+        JSONObject jsonLevel = null;
+        try {
+            jsonLevel = JsonParser.getFileContent(this, "main.json");
+            jsonLevel = JsonParser.getFileContent(this, jsonLevel.getJSONArray("levels").getString(0));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //defining all the elements the player
+        Player player = null;
+        DecorsElement decorsElement = null;
+        try {
+            //setting the player
+            JSONArray playerpos = jsonLevel.getJSONObject("player").getJSONArray("start_pos");
+            player = new Player(new Vector2D(playerpos.getInt(0),playerpos.getInt(1)),256,256);
+            player.setSprite(FileUtils.getFile(this, jsonLevel.getJSONObject("player").getString("sprite")));
+            mLevel.add(player);
+
+            //setting the decors elements list
+            JSONArray decorsArray = jsonLevel.getJSONArray("decorelements");
+            for (int i = 0 ; i < decorsArray.length(); i++) {
+                JSONObject decor = decorsArray.getJSONObject(i);
+                decorsElement = new DecorsElement(new Vector2D(0,0),decor.getJSONArray("size").getInt(0),decor.getJSONArray("size").getInt(1));
+                decorsElement.setSprite(FileUtils.getFile(this,decor.getString("sprite")));
+
+                mLevel.add(decorsElement);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+
 
     }
 
---
+++ src/main/java/com/herdeliaslegacy/runnergameengine/Model/DecorsElement.java
@@ -0,0 +1,14 @@
+package com.herdeliaslegacy.runnergameengine.Model;
+
+/**
+ * Created by skad on 16/09/15.
+ */
+public class DecorsElement extends SpriteObject {
+    public DecorsElement(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+    public DecorsElement(Vector2D pos, int width, int height) {
+        super(pos, width, height);
+    }
+}
