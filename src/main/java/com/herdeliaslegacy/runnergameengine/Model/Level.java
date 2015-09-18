38a140371d6c577b9cff5a11d882ad8eb1a4bd57
skad <skadprog@gmail.com>
2015-09-18 13:05:30 +0200

    Add element at the beginning of the screen for making infinity loop

--- src/main/java/com/herdeliaslegacy/runnergameengine/Model/Level.java
+++ src/main/java/com/herdeliaslegacy/runnergameengine/Model/Level.java
@@ -65,7 +65,7 @@ public class Level extends Observable {
     /**
      * Retrieve all the sprites!
      * Note: The order matters as the first object is drawn first (followers will be "above")
-     *
+     * Todo: need to find a better way to serve files because it will be quickly ram hungry
      * @return An (Array)List containing all sprites
      */
     public List<SpriteObject> getAllSprites() {
@@ -160,10 +160,18 @@ public class Level extends Observable {
             DecorsElement decorelement = i.next();
             decorelement.forward();
             i.set(decorelement);
+
+            //remove element who are off the screen
             if (decorelement.getXPos() + decorelement.getWidth() < 0) {
                 i.remove();
                 Log.d(TAG, "updateDecors mDecors size " + mDecors.size());
             }
+        }
+
+        //add element off the screen for the infinity loop
+        DecorsElement last = mDecors.get(mDecors.size()-1);
+        if (last.getXPos()+last.getWidth() < mMaxXDraw){
+            addRandomElementToDecor(mMaxXDraw);
         }
     }
 
