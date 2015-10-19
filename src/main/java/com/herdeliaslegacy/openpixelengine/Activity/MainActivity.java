59f7610ee2bdbef5d48d232daf01358e0ce21b24
skad <skadprog@gmail.com>
2015-10-19 17:44:43 +0200

    Cleaning import

--- src/main/java/com/herdeliaslegacy/openpixelengine/Activity/MainActivity.java
+++ src/main/java/com/herdeliaslegacy/openpixelengine/Activity/MainActivity.java
@@ -3,7 +3,6 @@ package com.herdeliaslegacy.openpixelengine.Activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
