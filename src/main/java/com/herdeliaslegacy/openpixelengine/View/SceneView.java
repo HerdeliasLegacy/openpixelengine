a55d54a5a2abb59212fbdfc2ee1223a78b85b8fe
skad <skadprog@gmail.com>
2016-01-05 16:38:58 +0100

    Update comment

--- src/main/java/com/herdeliaslegacy/openpixelengine/View/SceneView.java
+++ src/main/java/com/herdeliaslegacy/openpixelengine/View/SceneView.java
@@ -34,14 +34,22 @@ import com.herdeliaslegacy.openpixelengine.Model.SpriteObject;
 import com.herdeliaslegacy.openpixelengine.Model.Vector2D;
 
 /**
- * Created by skad on 08/09/15.
+ * Surface view for drawing the scene
+ * @author skad
+ * @date 08/09/15.
  */
 public class SceneView extends SurfaceView implements SurfaceHolder.Callback {
 
-    private static final String TAG = "LevelView";
-    private DisplayMetrics mScreenSize=null;
+    private static final String TAG = "LevelView"; // Tag for login echo
+    private DisplayMetrics mScreenSize=null;       // Metrics of the screen
 
 
+    /**
+     * Constructor
+     *
+     * @param context Parent activity
+     * @param attributeSet Params activity
+     */
     public SceneView(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
         SurfaceHolder holder = super.getHolder();
@@ -55,17 +63,26 @@ public class SceneView extends SurfaceView implements SurfaceHolder.Callback {
     }
 
 
+    /**
+     * Set the metric of the screen
+     * @param metric from the activity
+     */
     public void setScreenMetric(DisplayMetrics metric){
         this.mScreenSize = metric;
     }
+
+    /**
+     * General drawing method.
+     * Can call multiple drawing method
+     * @param scene scene to draw
+     */
     public void drawScene(Scene scene){
         drawSpriteElements(scene);
-
     }
 
     /**
      * Draw the scene's content in our view
-     * @param scene what to draw
+     * @param scene scene to draw
      */
     private void drawSpriteElements(Scene scene) {
         SurfaceHolder holder =  this.getHolder();
@@ -82,8 +99,8 @@ public class SceneView extends SurfaceView implements SurfaceHolder.Callback {
 
     /**
      * Compute the screen position for the Sprite pass into params
-     * @param spriteObject
-     * @return
+     * @param spriteObject to compute
+     * @return new position
      */
     private Vector2D computeScreenPosition(SpriteObject spriteObject){
         Vector2D position = new Vector2D(0,0);
