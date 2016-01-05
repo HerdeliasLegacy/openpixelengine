1b9bac6d7a68a6513f92c32fa2facbd8c33acc8f
skad <skadprog@gmail.com>
2016-01-05 17:22:20 +0100

    Update comment

--- src/main/java/com/herdeliaslegacy/openpixelengine/Utils/UiUtils.java
+++ src/main/java/com/herdeliaslegacy/openpixelengine/Utils/UiUtils.java
@@ -22,55 +22,109 @@ import android.content.Context;
 import android.view.View;
 import android.widget.FrameLayout;
 
+/**
+ * Static class with somme utilities methods for setting element for the ui
+ * this is the implementation of the IUiElement
+ *
+ * @author skad
+ * @date 04/12/2015
+ */
 public class UiUtils {
     private UiUtils() {
     }
 
-
+    /**
+     * Set the id of the element
+     * @param element element to set
+     * @param name id to set
+     */
     public static void setId(View element,String name){
         int id = element.getResources().getIdentifier(name, "id", element.getContext().getPackageName());
         element.setId(id);
     }
+
+    /**
+     * Set the layout of the element
+     * @param element element to set
+     */
     public static void setLayout(View element){
         FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
         layoutParams.topMargin = 0;
         layoutParams.leftMargin = 0;
         element.setLayoutParams(layoutParams);
     }
+
+    /**
+     * Set the position of the element
+     * @param element element to set
+     * @param x pos
+     * @param y pos
+     */
     public static void setPos(View element,int x, int y) {
         UiUtils.setX(element, x);
         UiUtils.setY(element, y);
     }
 
+    /**
+     * Set the x pos of the element
+     * @param element element to set
+     * @param x pos
+     */
     public static void setX(View element,int x) {
         FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) element.getLayoutParams();
         params.leftMargin = x;
         element.setLayoutParams(params);
     }
 
+    /**
+     * Set the y pos of the element
+     * @param element element to set
+     * @param y pos
+     */
     public static void setY(View element,int y) {
         FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) element.getLayoutParams();
         params.topMargin = y;
         element.setLayoutParams(params);
     }
 
+    /**
+     * Set texture of the element
+     * @param element element to set
+     * @param file filename of the texture, need a full path
+     */
     public static void setTexture(View element,String file) {
         Context context = element.getContext();
         int id = context.getResources().getIdentifier(file, "drawable", context.getPackageName());
         element.setBackgroundResource(id);
     }
 
+    /**
+     * Set the size of the element
+     * @param element element to set
+     * @param width of the element
+     * @param height of the element
+     */
     public static void setSize(View element,int width, int height){
         UiUtils.setWidth(element, width);
         UiUtils.setHeight(element, height);
     }
 
+    /**
+     * Set the with of the element
+     * @param element element to set
+     * @param width of the element
+     */
     public static void setWidth(View element,int width){
         FrameLayout.LayoutParams params = (FrameLayout.LayoutParams)element.getLayoutParams();
         params.width = width;
         element.setLayoutParams(params);
     }
 
+    /**
+     * Set the height of the element
+     * @param element to set
+     * @param height of the element
+     */
     public static void setHeight(View element,int height){
         FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) element.getLayoutParams();
         params.height = height;
