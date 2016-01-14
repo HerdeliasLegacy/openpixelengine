4b02cb3f6a25a3989f4ffc53d4b472a586b05af4
skad <skadprog@gmail.com>
2016-01-14 11:39:57 +0100

    Add and custom dialogbox with notitle and no decoration

---
+++ src/main/java/com/herdeliaslegacy/openpixelengine/Ui/CustomDialogBox.java
@@ -0,0 +1,42 @@
+/*
+ *  This file is part of  OpenPixelEngine.
+ *
+ *     OpenPixelEngine is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ *     OpenPixelEngine is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with OpenPixelEngine.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2016
+ */
+
+package com.herdeliaslegacy.openpixelengine.Ui;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.view.View;
+import android.view.Window;
+
+/**
+ * @author skad
+ * @date 14/01/16
+ */
+public class CustomDialogBox extends Dialog implements View.OnClickListener{
+    public CustomDialogBox(Context context) {
+        super(context);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
